1932c7630bf47347aa5cecd663a88f8314eedc0a
Nathan Neff <nathan.neff@gmail.com>
2010-12-05 19:21:28 -0600

    Ran into probs, so reigned it in a bit -- start back simple.

--- enrollioExample2.groovy
+++ enrollioExample2.groovy
@@ -1,13 +1,10 @@
 
 @Grapes([
-            @Grab("org.codehaus.geb:geb-core:latest.release"),
-            @Grab("org.seleniumhq.selenium:selenium-firefox-driver:latest.release")
+             @Grab("org.codehaus.geb:geb-core:latest.release"),
+             @Grab("org.seleniumhq.selenium:selenium-firefox-driver:latest.release")
 ])
 
 import geb.Browser
 Browser.drive("http://enrollio.org") {
-    $("input", name: "q").value("wikipedia")
-    $("input", value: "Search").click()
-    assert title.endsWith("Google Search")
-    assert $("li.g", 0).find("a.l").text() == "Wikipedia"
+    assert title == "Help - About"
 }
