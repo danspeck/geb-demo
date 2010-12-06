939dcc2c53ab4c0dc1439cbe36da3b693112cbde
Nathan Neff <nathan.neff@gmail.com>
2010-12-06 00:20:59 -0600

    Removed shebang line.

--- advancedTest.groovy
+++ advancedTest.groovy
@@ -1,5 +1,4 @@
 
-#!/bin/env groovy
 @Grapes([
               @Grab("org.codehaus.geb:geb-core:latest.release"),
               @Grab("org.seleniumhq.selenium:selenium-firefox-driver:latest.release")
