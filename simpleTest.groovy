b801588b6dbd11495ef566bcb2a75c785e42b746
Nathan Neff <nathan.neff@gmail.com>
2010-12-06 23:31:49 -0600

    Ensured that simpleTest works / looks good / exports successfully.

--- simpleTest.groovy
+++ simpleTest.groovy
@@ -1,12 +1,13 @@
 
 @Grapes([
-            @Grab("org.codehaus.geb:geb-core:latest.release"),
-            @Grab("org.seleniumhq.selenium:selenium-firefox-driver:latest.release")
+              @Grab("org.codehaus.geb:geb-core:latest.release"),
+              @Grab("org.seleniumhq.selenium:selenium-firefox-driver:latest.release")
 ])
-
 import geb.Browser
-Browser.drive("http://enrollio.org") {
-    assert title == "Help - About"
-    // Un-comment driver.close() to close the browser window
-    // driver.close()
+Browser.drive("http://enrollio.org") {  
+      assert title == "Help - About"
+      // Un-comment driver.close() to close the browser window
+      // driver.close()  
+      assert $("a", text:"Loooogin").size() == 0
+      assert $("a", text:"Login").size() == 1
 }
